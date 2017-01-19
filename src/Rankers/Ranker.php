6f085f7db2b0a74d90a844bae76e04245af19d11
Usama Ashraf <uashraf588@gmail.com>
2017-01-19 13:42:05 +0500

    Add the ranker class responsible for running all rankers

---
+++ src/Rankers/Ranker.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Wordranker\Rankers;
+
+
+class Ranker
+{
+
+    /** @var Converter[] */
+    protected $converters;
+
+    /**
+     * Ranker constructor.
+     *
+     * @param $blacklist array
+     * @param $regex
+     */
+    public function __construct($blacklist, $regex)
+    {
+        $this->converters = [
+            new BlackList($blacklist),
+            new Matcher($regex)
+        ];
+    }
+
+    /**
+     * @param $words array
+     * @return array
+     */
+    public function run($words)
+    {
+        $converters = $this->converters;
+
+        foreach ($converters as $converter)
+            $words = $converter->convert($words);
+
+        return $words;
+    }
+}
